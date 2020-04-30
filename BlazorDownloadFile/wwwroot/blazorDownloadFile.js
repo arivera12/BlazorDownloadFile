71a24683cb83c7eb54be5c1f6abc40269fd2dc02
Anthony Rivera <anthony_elgueta@hotmail.com>
2020-04-30 14:59:03 -0400

    Added support for Edge browser.

--- BlazorDownloadFile/wwwroot/blazorDownloadFile.js
+++ BlazorDownloadFile/wwwroot/blazorDownloadFile.js
@@ -1,9 +1,21 @@
 window.downloadFile = function (filename, bytesBase64) {
-    var link = document.createElement('a');
-    link.download = filename;
-    link.style.display = "none";
-    link.href = "data:application/octet-stream;base64," + bytesBase64;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    if (navigator.msSaveBlob) {
+        //Download document in Edge browser
+        var data = window.atob(bytesBase64);
+        var bytes = new Uint8Array(data.length);
+        for (var i = 0; i < data.length; i++) {
+            bytes[i] = data.charCodeAt(i);
+        }
+        var blob = new Blob([bytes.buffer], { type: "application/octet-stream" });
+        navigator.msSaveBlob(blob, filename);
+    }
+    else {
+        var link = document.createElement('a');
+        link.download = filename;
+        link.style.display = "none";
+        link.href = "data:application/octet-stream;base64," + bytesBase64;
+        document.body.appendChild(link); // Needed for Firefox
+        link.click();
+        document.body.removeChild(link);
+    }
 };
