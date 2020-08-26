490ef66e9eb928c454934de92ff37887865ddc4a
Anthony Rivera <arivera@amcgroup.pr>
2020-08-26 13:52:25 -0400

    Fix https://github.com/arivera12/BlazorDownloadFile/issues/20.

--- BlazorDownloadFile/Extensions/ServiceCollectionExtensions.cs
+++ BlazorDownloadFile/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.JSInterop;
 
 namespace BlazorDownloadFile
 {
@@ -8,10 +10,14 @@ namespace BlazorDownloadFile
         /// Registers blazor download file service
         /// </summary>
         /// <param name="services"></param>
+        /// <param name="lifetime"></param>
         /// <returns></returns>
-        public static IServiceCollection AddBlazorDownloadFile(this IServiceCollection services)
+        public static IServiceCollection AddBlazorDownloadFile(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Scoped)
         {
-            return services.AddScoped<IBlazorDownloadFileService, BlazorDownloadFileService>();
+            return ServiceCollectionDescriptorExtensions.Add(services, 
+                new ServiceDescriptor(typeof(IBlazorDownloadFileService), 
+                sp => new BlazorDownloadFileService(sp.GetRequiredService<IJSRuntime>()), 
+                lifetime));
         }
     }
 }
